5b8d6126804f1112fc8c01d58ccb110ab6f81340
varunnair1234 <varunnair304@gmail.com>
2026-08-05 12:53:47 -0700

    loop update

--- loop.py
+++ loop.py
@@ -1,15 +1,33 @@
 #Loop
 
 import cv2
+import numpy as np
 
-webcam = cv2.VideoCapture(0)
+webcam = cv2.VideoCapture(0)        # starts up the webcam of the computer being used
 
-if webcam.isOpened():
+if webcam.isOpened():               # sanity check
     print('yes')
 
 else:
-    print(f'error')
+    print('error')
     exit()
+
+ret, frame = webcam.read()          # reads first frame of the webcam
+
+if not ret:
+    print(f'read failed, ret = {ret}')
+    exit()
+
+x, y, w, h = cv2.selectROI('Select Object', frame)
+
+patch = frame[y:y+h, x:x+w]
+
+patch = cv2.cvtColor(patch, cv2.COLOR_BGR2GRAY)
+
+patch = cv2.resize(patch, (32, 32))
+patch_arr = patch.flatten()
+
+print(patch_arr.shape)
 
 while True:
     ret, frame = webcam.read()
