63ca26e90886472073676785fc06dbb99dd10e26
varunnair1234 <varunnair304@gmail.com>
2026-08-05 12:58:21 -0700

    added two functions to class AppearanceModel

---
+++ model.py
@@ -0,0 +1,12 @@
+class AppearanceModel():
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.mean = None
+        self.components = None
+        self.singular_values = None
+        self.n_samples = 0
+    
+    def initialize(self, patch):
+        self.mean = patch
+        self.n_samples = 1
+
