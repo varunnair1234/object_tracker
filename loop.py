4d4aeab70a7271c30aa7b98e020eb740ac6a73b8
varunnair1234 <varunnair304@gmail.com>
2026-08-04 22:34:45 -0700

    loop.py

---
+++ loop.py
@@ -0,0 +1,23 @@
+import cv2
+
+webcam = cv2.VideoCapture(0)
+
+if webcam.isOpened():
+    print('yes')
+elif not webcam.isOpened():
+    print(f'Error')
+
+while True:
+    ret, frame = webcam.read()
+
+    if not ret:
+        break
+
+    cv2.imshow('Tracker', frame)
+
+    key = cv2.waitKey(30) & 0xFF
+    if key == ord('q'):
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
